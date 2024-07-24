5502dc6f2ac624cfe5723c99998434a5ba6d70fb
Tarun Mali <tarunmalibackup2@gmail.comm>
2024-07-24 10:09:49 +0530

    Initial commit

---
+++ src/main/java/com/example/searchTypeAheadFinal/SearchTypeAheadFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.searchTypeAheadFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchTypeAheadFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchTypeAheadFinalApplication.class, args);
+	}
+
+}
